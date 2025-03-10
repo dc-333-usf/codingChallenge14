d43b48d4464210f68f6097c689dac505ead826c7
Drew Cook <dc333@usf.edu>
2025-03-09 21:53:14 -0400

    Task 2 - Support Tickets Dynamic Addition

--- cc_14.js
+++ cc_14.js
@@ -1,1 +1,35 @@
 //Task 2: adding support tickets dynamically.
+allTickets = [];
+
+function createTicket(name, description, priority) {
+    let ticketContainer = document.getElementById("ticketContainer");
+    let ticket = document.createElement(`div`);
+
+    ticket.setAttribute(`class`, `ticket`);
+    ticket.setAttribute(`id`, `${allTickets.length + 1}`);
+
+    ticket.style.backgroundColor = "lightblue";
+    ticket.style.width = "33.33%";
+    ticket.style.textAlign = "center";
+    ticket.style.borderRadius = "10px";
+    ticket.innerHTML = `
+    <h3>Name: ${name}</h3>
+    <p>Issue: ${description}</p>
+    <label>Priority: ${priority}</label>
+    <button class="resolveButton">Resolve</button>`;
+
+    allTickets.push(ticket);
+    ticketContainer.appendChild(ticket);
+
+    const resolveButton = ticket.querySelector(".resolveButton");
+    resolveButton.addEventListener("click", () => {
+        ticketContainer.removeChild(ticket);
+    });
+
+}
+
+createTicket("Jake", "My computer broke", "High");
+createTicket("Finn", "My computer exploded", "Medium");
+createTicket("Patrick", "My computer caught fire", "Medium");
+createTicket("Subaru", "My computer has a virus", "Low");
+createTicket("Matt", "My computer smells", "High");
