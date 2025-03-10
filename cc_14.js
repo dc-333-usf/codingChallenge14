4e34fd6658939be9dea93837521ec1eb4c01f419
Drew Cook <dc333@usf.edu>
2025-03-09 22:14:35 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -5,7 +5,7 @@ function createTicket(name, description, priority) {
     let ticketContainer = document.getElementById("ticketContainer");
     let ticket = document.createElement(`div`);
 
-    ticket.setAttribute(`class`, `ticket`);
+    ticket.setAttribute(`class`, `${priority}`);
     ticket.setAttribute(`id`, `${allTickets.length + 1}`);
 
     ticket.style.backgroundColor = "lightblue";
@@ -21,11 +21,6 @@ function createTicket(name, description, priority) {
     allTickets.push(ticket);
     ticketContainer.appendChild(ticket);
 
-    const resolveButton = ticket.querySelector(".resolveButton");
-    resolveButton.addEventListener("click", () => {
-        ticketContainer.removeChild(ticket);
-    });
-
 }
 
 createTicket("Jake", "My computer broke", "High");
@@ -33,3 +28,11 @@ createTicket("Finn", "My computer exploded", "Medium");
 createTicket("Patrick", "My computer caught fire", "Medium");
 createTicket("Subaru", "My computer has a virus", "Low");
 createTicket("Matt", "My computer smells", "High");
+
+
+//Task 3: converting NodeLists to arrays for bulk updates.
+const allHighTickets = Array.from(ticketContainer.querySelectorAll('.High'));
+allHighTickets.forEach(n => {
+    n.style.backgroundColor = "red";
+})
+
